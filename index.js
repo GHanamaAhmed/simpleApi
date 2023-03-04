2ceae7a4179b3aaf5494477e6eb2bc0d9fda7a8f
ghanama ahmed <54191980+GHanamaAhmed@users.noreply.github.com>
2023-03-04 22:12:18 +0100

    v2

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const { schemaUser } = require('./validate/validate');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const app = express();
- mongoose.set("strictQuery",false)
-const connectDB=async ()=>{
+mongoose.set("strictQuery", false)
+const connectDB = async () => {
     try {
-        con=await mongoose.connect(process.env.MONGO_URI)
+        con = await mongoose.connect(process.env.MONGO_URI)
         console.log("Connect succssful");
     } catch (error) {
         console.log(error);
@@ -20,29 +20,34 @@ const PORT = 8080;
 app.use(helmet())
 app.use(bodyPparser.urlencoded({ extended: true }))
 app.post("/users/signup", async (req, res) => {
-    const { value, error } = schemaUser.validate(req.body)
-    if (error) {
-        res.json({
-            res:false,
-            mes:error.message
-        })
+    const { value, err } = schemaUser.validate(req.body)
+    try {
+        if (err) {
+            res.json({
+                res: false,
+                mes: err.message
+            })
 
-    } else if ((await Users.find({ email: req.body.email })).length > 0) {
-        res.json({
-            res: false,
-            mes: "Email is exist!"
-        })
-    } else {
-        let user = new Users(req.body)
-        await user.save()
-        res.json({
-            res: true,
-            mes: "Succeeful"
-        })
+        } else if ((await Users.find({ email: req.body.email })).length > 0) {
+            res.json({
+                res: false,
+                mes: "Email is exist!"
+            })
+        } else {
+            let user =  new Users(req.body)
+            await user.save()
+            res.json({
+                res: true,
+                mes: "Succeeful"
+            })
+        }
+    } catch (error) {
+        console.log(error);
     }
 
+
 })
-connectDB().then(()=>{
+connectDB().then(() => {
     app.listen(process.env.PORT || PORT)
 })
 
